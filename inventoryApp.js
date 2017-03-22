cf2b379e655c8f364cfe155c610924e11b219450
ashlei <ashlei.jones@detroitlabs.com>
2017-03-22 15:02:11 -0400

    adding two item objects to inventory object

--- inventoryApp.js
+++ inventoryApp.js
@@ -6,4 +6,18 @@ var prompt = readline.createInterface({
 });
 
 var storeInventory = {
+
+	item1 : {
+		name: "book",
+		description: "hold this and look smrt",
+		price: 5.50,
+		quantity: 10
+	},
+
+	item2 : {
+		name: "samurai sword",
+		description: "ladies love samurai swords",
+		price: 500,
+		quantity: 2
+	}
 };
